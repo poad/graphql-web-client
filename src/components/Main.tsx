96e3fcb63f27914ba9c74572d0004c7f8beb15a6
Kenji Saito <ken-yo@mbr.nifty.com>
2022-05-14 06:08:21 +0900

    Migrate chakra-ui to v2

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,7 +4,7 @@ import { Center, VStack, Stack, StackProps, Heading } from '@chakra-ui/react';
 export const Main = (props: StackProps): JSX.Element => (
   <Center w='80%' minW='85%'>
     <VStack w='100%'>
-      <Heading pt='1rem' size="2xl" isTruncated>GraphQL Online Client</Heading>
+      <Heading pt='1rem' size="2xl" noOfLines={1}>GraphQL Online Client</Heading>
       <Stack
         id='main'
         w='80%'
