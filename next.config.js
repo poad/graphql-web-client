c5a1529de7b382529b9483474cff406a46ecd5ef
Kenji Saito <ken-yo@mbr.nifty.com>
2022-05-15 21:56:03 +0900

    Add the analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-    webpack5: true,
-    reactStrictMode: true,
-    esmExternals: true,
-    swcLoader: true,
-    swcMinify: true,
-};
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withPlugins([
+    [withBundleAnalyzer],
+],
+    {
+        webpack5: true,
+        reactStrictMode: true,
+        esmExternals: true,
+        swcLoader: true,
+        swcMinify: true,
+        experimental: {
+            modern: true,
+        }
+    }
+);
