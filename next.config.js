8f99a038e0fbe555300dd9cbc8c457b4bfa53887
Kenji Saito <ken-yo@mbr.nifty.com>
2023-05-03 10:54:44 +0900

    Bumps node modules

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer');
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  transpilePackages: ['@mui/system', '@mui/material', '@mui/icons-material'],
-  modularizeImports: {
-    '@mui/material/?(((\\w*)?/?)*)': {
-      transform: '@mui/material/{{ matches.[1] }}/{{member}}',
-    },
-    '@mui/icons-material/?(((\\w*)?/?)*)': {
-      transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}',
-    },
-  },
 };
 
 module.exports = withBundleAnalyzer({
