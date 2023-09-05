ebd38a7f46e09fc9a0841634871cbab9581732e2
Kenji Saito <ken-yo@mbr.nifty.com>
2023-09-06 07:57:04 +0900

    Bumps node modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
-'use client';
-import { ReactNode } from 'react';
-import './globals.css';
-import StyledJsxRegistry from './registry';
+"use client";
+import { ReactNode } from "react";
+import "./globals.css";
+import StyledJsxRegistry from "./registry";
 
 export default function Document({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <title>GraphQL Client</title>
-        <meta name='description' content='GraphQL Client' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </head>
-      <body><StyledJsxRegistry>{children}</StyledJsxRegistry></body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<title>GraphQL Client</title>
+				<meta name="description" content="GraphQL Client" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</head>
+			<body>
+				<StyledJsxRegistry>{children}</StyledJsxRegistry>
+			</body>
+		</html>
+	);
 }
