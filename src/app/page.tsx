fe2dd3493e752eb10721abecbb146931c317822f
Kenji Saito <ken-yo@mbr.nifty.com>
2024-03-12 07:10:08 +0900

    Bumps node modules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState } from "react";
 import Box from "@mui/material/Box";
 import { createGraphiQLFetcher } from "@graphiql/toolkit";
--- src/app/registry.tsx
+++ src/app/registry.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { ReactNode, useState } from "react";
+import type { ReactNode } from "react";
+import { useState } from "react";
 import { useServerInsertedHTML } from "next/navigation";
 import { StyleRegistry, createStyleRegistry } from "styled-jsx";
 
