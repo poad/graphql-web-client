ebdd39bd554615fe9ac4f1a9043e7483a663a5db
Kenji Saito <ken-yo@mbr.nifty.com>
2024-09-13 00:16:51 +0900

    Bumps node modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,17 +4,17 @@ import "./globals.css";
 import StyledJsxRegistry from "./registry";
 
 export default function Document({ children }: { children: ReactNode }) {
-	return (
-		<html lang="en">
-			<head>
-				<title>GraphQL Client</title>
-				<meta name="description" content="GraphQL Client" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
-			</head>
-			<body>
-				<StyledJsxRegistry>{children}</StyledJsxRegistry>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        <title>GraphQL Client</title>
+        <meta name="description" content="GraphQL Client" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>
+        <StyledJsxRegistry>{children}</StyledJsxRegistry>
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,36 +11,36 @@ import "graphiql/graphiql.min.css";
 const GraphiQLProvider = dynamic(() => import("graphiql"), { ssr: false });
 
 export default function Home(): JSX.Element {
-	const [url, setUrl] = useState<string>();
+  const [url, setUrl] = useState<string>();
 
-	const fetcher = createGraphiQLFetcher({
-		url: url || "https://beta.pokeapi.co/graphql/v1beta",
-		fetch,
-	});
+  const fetcher = createGraphiQLFetcher({
+    url: url || "https://beta.pokeapi.co/graphql/v1beta",
+    fetch,
+  });
 
-	return (
-		<Box sx={{ w: "100vmax", h: "100vmin" }} alignItems="flex-start">
-			<TextField
-				name={"endpoint"}
-				placeholder="GraphQL endpoint"
-				sx={{
-					minWidth: "calc(100vmax - 2rem)",
-					fontSize: "14",
-					padding: "0",
-					marginTop: "1rem",
-					marginLeft: "1rem",
-				}}
-				value={url}
-				onChange={async (
-					event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-				) => setUrl(event.target.value)}
-			/>
+  return (
+    <Box sx={{ w: "100vmax", h: "100vmin" }} alignItems="flex-start">
+      <TextField
+        name={"endpoint"}
+        placeholder="GraphQL endpoint"
+        sx={{
+          minWidth: "calc(100vmax - 2rem)",
+          fontSize: "14",
+          padding: "0",
+          marginTop: "1rem",
+          marginLeft: "1rem",
+        }}
+        value={url}
+        onChange={async (
+          event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+        ) => setUrl(event.target.value)}
+      />
 
-			<div style={{ height: "90vh", width: "100vmax" }}>
-				<GraphiQLProvider fetcher={fetcher}>
-					<GraphiQLInterface />
-				</GraphiQLProvider>
-			</div>
-		</Box>
-	);
+      <div style={{ height: "90vh", width: "100vmax" }}>
+        <GraphiQLProvider fetcher={fetcher}>
+          <GraphiQLInterface />
+        </GraphiQLProvider>
+      </div>
+    </Box>
+  );
 }
