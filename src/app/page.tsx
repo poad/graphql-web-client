285b73b9049b6fc08c2a128d6a192cd264f2f1dd
Kenji Saito <ken-yo@mbr.nifty.com>
2024-12-10 11:31:55 +0900

    Bumps node modules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { JSX, useState } from 'react';
 import Box from '@mui/material/Box';
 import { createGraphiQLFetcher } from '@graphiql/toolkit';
 import { TextField } from '@mui/material';
